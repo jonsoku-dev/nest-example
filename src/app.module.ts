e6cf2cd6bafdcee6774ef416b0419f2bdc4808fd
jonsoku2 <whdtjr2792@gmail.com>
2020-11-22 00:19:59 +0900

    nestjs 모듈 캡슐화

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { CoffeesModule } from './coffees/coffees.module';
       }),
     }),
     CoffeesModule,
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],

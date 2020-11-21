8b1e33c89eed2111a06aea8f7d3ae4ae218bd779
jonsoku2 <whdtjr2792@gmail.com>
2020-11-22 00:22:12 +0900

    가치기반제공자

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,13 +6,15 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
+class MockCoffeesService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     {
       provide: CoffeesService,
-      useClass: CoffeesService,
+      useValue: new MockCoffeesService()
     },
   ],
   exports: [CoffeesService],
